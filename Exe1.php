2e6d606a9cd5c36b315fe731bb68827b4f1d2f08
JuniorIkee <50925428+JuniorIkee@users.noreply.github.com>
2021-06-30 22:03:35 -0300

    Create Exe1.php

---
+++ Exe1.php
@@ -0,0 +1,22 @@
+<? php
+
+
+$ num =   $ _GET [ "numero" ];
+
+if ( $ num % 2 == 0 )
+{
+    echo  "$ numero é par!" ;
+}
+else {
+    echo  "$ numero é ímpar!" ;
+}
+
+if ( $ num > = 0 )
+{
+    echo  "<br/>$ numero é positivo!" ;
+}
+else {
+    echo  "<br/>$ numero é negativo!" ;
+}
+   
+?>
