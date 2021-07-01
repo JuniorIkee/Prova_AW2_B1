1d9664c46ccb3c0e6ae1791c1f7d4c503d54ea36
JuniorIkee <50925428+JuniorIkee@users.noreply.github.com>
2021-06-30 22:07:20 -0300

    Create exe2.php

---
+++ exe2.php
@@ -0,0 +1,19 @@
+<? php
+$ n =   $ _POST [ "n" ];
+$ k =   $ _POST [ "k" ];
+
+$ array = [ $ n , $ k ];
+$ i = 0 ;
+$ j = 0 ;
+
+enquanto ( $ i < $ array [ 1 ])
+{
+    echo  "" ;
+    $ j = $ n * $ i ;
+    echo  $ j ;
+    $ i ++;
+
+    $ j = 0 ;
+}
+
+?>
